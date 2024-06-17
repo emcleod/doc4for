2cfacd0b35ae7ec1e31b560bbf9280164f0807eb
Elaine McLeod <em.mcleod@gmail.com>
2024-06-17 12:44:15 +0100

    Adding a class to represent the file structure

--- tests/f90/test_build_directory_tree.py
+++ tests/f90/test_build_directory_tree.py
@@ -1,7 +1,7 @@
 import unittest
 from pathlib import Path
 from pyfakefs.fake_filesystem_unittest import TestCase
-from f90.generate_file_tree import build_directory_tree  # Import your function
+from f90.generate_file_tree import build_directory_tree, DirectoryTree
 
 class TestBuildDirectoryTree(TestCase):
     def setUp(self):
@@ -10,33 +10,42 @@ class TestBuildDirectoryTree(TestCase):
     def test_empty_list(self):
         """Test with an empty list of files."""
         result = build_directory_tree([])
-        self.assertEqual(result, {})
+        expected = DirectoryTree('')
+        
+        self.assertEqual(list(result.walk()), list(expected.walk()))
 
     def test_single_file(self):
         """Test with a single file in the path directory."""
         self.fs.create_file('/path/file.f90')
         result = build_directory_tree(['/path/file.f90'])
-        expected = {'/': {
-                'path': {
-                    'file.f90': '/path/file.f90'
-                }
-            }
-        }
-        self.assertEqual(result, expected)
+        expected = DirectoryTree('')
+        root = DirectoryTree('/', expected)
+        expected.children.append(root)
+        path_dir = DirectoryTree('path', root)
+        root.children.append(path_dir)
+        path_dir.children.append('/path/file.f90')
+        actual_paths = list(result.walk())
+        expected_paths = list(expected.walk())
+
+        self.assertEqual(actual_paths, expected_paths)
 
     def test_multiple_files_same_directory(self):
         """Test with multiple files in the same directory."""
         self.fs.create_file('/path/file1.f90')
         self.fs.create_file('/path/file2.f90')
         result = build_directory_tree(['/path/file1.f90', '/path/file2.f90'])
-        expected = {'/': {
-                'path': {
-                    'file1.f90': '/path/file1.f90',
-                    'file2.f90': '/path/file2.f90'
-                }
-            }
-        }
-        self.assertEqual(result, expected)
+        expected = DirectoryTree('')
+        root = DirectoryTree('/', expected)
+        expected.children.append(root)
+        path_dir = DirectoryTree('path', root)
+        root.children.append(path_dir)
+        path_dir.children.append('/path/file1.f90')
+        path_dir.children.append('/path/file2.f90')
+
+        actual_paths = list(result.walk())
+        expected_paths = list(expected.walk())
+
+        self.assertEqual(actual_paths, expected_paths)
 
     def test_nested_directories(self):
         """Test with files in nested directories."""
@@ -49,21 +58,30 @@ class TestBuildDirectoryTree(TestCase):
             '/path/dir2/file3.f90'
         ]
         result = build_directory_tree(files)
-        expected = {'/': {
-                'path': {
-                    'dir1': {
-                        'file1.f90': '/path/dir1/file1.f90',
-                        'subdir': {
-                            'file2.f90': '/path/dir1/subdir/file2.f90'
-                        }
-                    },
-                    'dir2': {
-                        'file3.f90': '/path/dir2/file3.f90'
-                    }
-                }
-            }
-        }
-        self.assertEqual(result, expected)
+
+        expected = DirectoryTree('')
+        root = DirectoryTree('/', expected)
+        expected.children.append(root)
+        
+        path = DirectoryTree('path', root)
+        root.children.append(path)
+        
+        dir1 = DirectoryTree('dir1', path)
+        path.children.append(dir1)
+        dir1.children.append('/path/dir1/file1.f90')
+        
+        subdir = DirectoryTree('subdir', dir1)
+        dir1.children.append(subdir)
+        subdir.children.append('/path/dir1/subdir/file2.f90')
+        
+        dir2 = DirectoryTree('dir2', path)
+        path.children.append(dir2)
+        dir2.children.append('/path/dir2/file3.f90')
+
+        actual_paths = list(result.walk())
+        expected_paths = list(expected.walk())
+
+        self.assertEqual(actual_paths, expected_paths)
 
     def test_files_with_same_name(self):
         """Test with files having the same name in different directories."""
@@ -71,14 +89,19 @@ class TestBuildDirectoryTree(TestCase):
         self.fs.create_file('/path/dir2/file.f90')
         files = ['/path/dir1/file.f90', '/path/dir2/file.f90']
         result = build_directory_tree(files)
-        expected = {'/': {
-                'path': {
-                    'dir1': {'file.f90': '/path/dir1/file.f90'},
-                    'dir2': {'file.f90': '/path/dir2/file.f90'}
-                }
-            }
-        }
-        self.assertEqual(result, expected)
+
+        expected = DirectoryTree('')
+        root = DirectoryTree('/', expected)
+        expected.children.append(root)
+        path = DirectoryTree('path', root)
+        root.children.append(path)
+        dir1 = DirectoryTree('dir1', path)
+        dir2 = DirectoryTree('dir2', path)
+        path.children.extend([dir1, dir2])
+        dir1.children.append('/path/dir1/file.f90')
+        dir2.children.append('/path/dir2/file.f90')
+
+        self.assertEqual(list(result.walk()), list(expected.walk()))
 
     def test_non_f90_files(self):
         """Test that non-f90 files are handled correctly."""
@@ -86,14 +109,15 @@ class TestBuildDirectoryTree(TestCase):
         self.fs.create_file('/path/file.txt')
         files = ['/path/file.f90', '/path/file.txt']
         result = build_directory_tree(files)
-        expected = {'/': {
-                'path': {
-                    'file.f90': '/path/file.f90',
-                    'file.txt': '/path/file.txt'
-                }
-            }
-        }
-        self.assertEqual(result, expected)
+
+        expected = DirectoryTree('')
+        root = DirectoryTree('/', expected)
+        expected.children.append(root)
+        path = DirectoryTree('path', root)
+        root.children.append(path)
+        path.children.extend(['/path/file.f90', '/path/file.txt'])
+
+        self.assertEqual(list(result.walk()), list(expected.walk()))
 
     def test_absolute_and_relative_paths(self):
         """Test with a mix of absolute and relative paths."""
@@ -101,16 +125,23 @@ class TestBuildDirectoryTree(TestCase):
         self.fs.create_file('rel/path/file2.f90')
         files = ['/abs/path/file1.f90', 'rel/path/file2.f90']
         result = build_directory_tree(files)
-        expected = {'/': {
-                'abs': {
-                    'path': {'file1.f90': '/abs/path/file1.f90'}
-                }
-            },
-            'rel': {
-                'path': {'file2.f90': 'rel/path/file2.f90'}
-            }
-        }
-        self.assertEqual(result, expected)
+
+        expected = DirectoryTree('')
+        root = DirectoryTree('/', expected)
+        expected.children.append(root)
+        abs_dir = DirectoryTree('abs', root)
+        root.children.append(abs_dir)
+        abs_path = DirectoryTree('path', abs_dir)
+        abs_dir.children.append(abs_path)
+        abs_path.children.append('/abs/path/file1.f90')
+
+        rel = DirectoryTree('rel', expected)
+        expected.children.append(rel)
+        rel_path = DirectoryTree('path', rel)
+        rel.children.append(rel_path)
+        rel_path.children.append('rel/path/file2.f90')
+
+        self.assertEqual(list(result.walk()), list(expected.walk()))
 
     def test_empty_directories(self):
         """Test that empty directories are not included."""
@@ -118,13 +149,17 @@ class TestBuildDirectoryTree(TestCase):
         self.fs.create_dir('/path/empty_dir')
         files = ['/path/dir1/file.f90']
         result = build_directory_tree(files)
-        expected = {'/': {
-                'path': {
-                    'dir1': {'file.f90': '/path/dir1/file.f90'}
-                }
-            }
-        }
-        self.assertEqual(result, expected)
+
+        expected = DirectoryTree('')
+        root = DirectoryTree('/', expected)
+        expected.children.append(root)
+        path = DirectoryTree('path', root)
+        root.children.append(path)
+        dir1 = DirectoryTree('dir1', path)
+        path.children.append(dir1)
+        dir1.children.append('/path/dir1/file.f90')
+
+        self.assertEqual(list(result.walk()), list(expected.walk()))
 
     def test_mixed_separators(self):
         """Test with file paths using different separators."""
@@ -135,48 +170,19 @@ class TestBuildDirectoryTree(TestCase):
             r'\path\dir2\file2.f90'  # Using Windows-style path
         ]
         result = build_directory_tree(files)
-        expected = {
-            '/': {
-                'path': {
-                    'dir1': {'file1.f90': str(Path('/path/dir1/file1.f90'))},
-                    'dir2': {'file2.f90': str(Path('/path/dir2/file2.f90'))}  # Updated expected path
-                }
-            }
-        }
-        self.assertEqual(result, expected)
-    # def test_mixed_separators(self):
-    #     """Test with file paths using different separators."""
-    #     self.fs.create_file('/path/dir1/file1.f90')
-    #     self.fs.create_file('/path/dir2/file2.f90')
-    #     files = [
-    #         '/path/dir1/file1.f90',
-    #         r'\path\dir2\file2.f90'  # Using Windows-style path
-    #     ]
-    #     result = build_directory_tree(files)
-    #     expected = {'/': {
-    #             'path': {
-    #                 'dir1': {'file1.f90': str(Path('/path/dir1/file1.f90'))},
-    #                 'dir2': {'file2.f90': str(Path(r'\path\dir2\file2.f90'))}
-    #             }
-    #         }
-    #     }
-    #     self.assertEqual(result, expected)
-    # def test_mixed_separators(self):
-    #     """Test with file paths using different separators."""
-    #     self.fs.create_file('/path/dir1/file1.f90')
-    #     self.fs.create_file('/path/dir2/file2.f90')
-    #     files = [
-    #         '/path/dir1/file1.f90',
-    #         r'\path\dir2\file2.f90'  # Using Windows-style path
-    #     ]
-    #     result = build_directory_tree(files)
-    #     expected = {
-    #         'path': {
-    #             'dir1': {'file1.f90': os.path.normpath('/path/dir1/file1.f90')},
-    #             'dir2': {'file2.f90': os.path.normpath(r'\path\dir2\file2.f90')}
-    #         }
-    #     }
-    #     self.assertEqual(result, expected)
+
+        expected = DirectoryTree('')
+        root = DirectoryTree('/', expected)
+        expected.children.append(root)
+        path = DirectoryTree('path', root)
+        root.children.append(path)
+        dir1 = DirectoryTree('dir1', path)
+        dir2 = DirectoryTree('dir2', path)
+        path.children.extend([dir1, dir2])
+        dir1.children.append('/path/dir1/file1.f90')
+        dir2.children.append('/path/dir2/file2.f90')
+
+        self.assertEqual(list(result.walk()), list(expected.walk()))
 
 if __name__ == '__main__':
     unittest.main()
